6794d81d3da0a25ec6dd596ce8b2f11c3e95343b
tomssmith17 <thsmith@clarku.edu>
2020-01-12 20:52:46 -0700

    obey your master

--- main.js
+++ main.js
@@ -3,4 +3,8 @@ var funksion = function(){
     var poo=1+1
     var please="WORK!"
     var hacking="hello git"
+    var kale="obey"
+    var chopstick="your"
+    var fork="master"
+    var spoon="spoon"
 }
